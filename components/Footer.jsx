a6a253d9f7438e5588620406196a7ea7b60173e5
Vedansh Shrivastava <vedanshwork302@gmail.com>
2026-02-24 15:47:00 +0530

    some changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -32,9 +32,9 @@ const Footer = () => {
         {
             title: "CONTACT",
             links: [
-                { text: "+1-212-456-7890", path: '/', icon: MailIcon },
+                { text: "+91-9999999999", path: '/', icon: MailIcon },
                 { text: "contact@example.com", path: '/', icon: PhoneIcon },
-                { text: "794 Francisco, 94102", path: '/', icon: MapPinIcon }
+                { text: "123 Main St, 62704", path: '/', icon: MapPinIcon }
             ]
         }
     ];
